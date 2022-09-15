e8e83eeb24147c92211816a16e35cbb8029479e0
Luniau <lubecarvalho@hotmail.com>
2022-09-15 16:20:22 +0200

    added API

---
+++ src/app.js
@@ -0,0 +1,4 @@
+let apiKey = "3c18d4acd1b30c24cd3ff99bbb182027";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=London&appid=3c18d4acd1b30c24cd3ff99bbb182027`;
+
+console.log(apiUrl)
